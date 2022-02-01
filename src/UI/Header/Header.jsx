f04ce8a99311d3208dbf25715a7c3165378c7f3d
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-02-01 05:20:11 +0200

    add routing, new styles, scss mixins and variables

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import styles from "./Header.module.scss";
 import user_photo from "../../IMG/user_photo.jpg";
 import dropdown_arrow from "../../IMG/dropdown_arrow.svg";
@@ -10,26 +11,26 @@ const Header = () => {
                 <div>
                     <select className={styles.dropdown} name="Organization">
                         <option value="school_1">SCHOOL 1</option>
-                        <option value="school_1">SCHOOL 2</option>
+                        <option value="school_2">SCHOOL 2</option>
                     </select>
                 </div>
                 <div>
-                    <span>ANALYTICS</span>
+                    <NavLink className={styles.headerNavLink} to="#">ANALYTICS</NavLink>
                 </div>
                 <div>
-                    <span>GRADEBOOKS</span>
+                    <NavLink className={styles.headerNavLink} to="#">GRADEBOOKS</NavLink>
                 </div>
                 <div>
-                    <span>TESTS</span>
+                    <NavLink className={styles.headerNavLink} to="#">TESTS</NavLink>
                 </div>
                 <div>
-                    <span>STUDENTS</span>
+                    <NavLink className={styles.headerNavLink} to="#">STUDENTS</NavLink>
                 </div>
                 <div>
-                    <span>TEACHERS</span>
+                    <NavLink className={styles.headerNavLink} to="#">TEACHERS</NavLink>
                 </div>
                 <div>
-                    <span>ARCHIVE</span>
+                    <NavLink className={styles.headerNavLink} to="#">ARCHIVE</NavLink>
                 </div>
                 <div className={styles.loginWrapp}>
                     <div className={styles.loginPhoto}><img src={user_photo} alt="user_photo"/></div>
