5e076a0cb3f68ada2cd755f6bc9f8e40b7f0a3f3
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-02-17 23:27:10 +0200

    refactor styles in header, add new mixins

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import styles from "./Header.module.scss";
 
-//import images / icons
+//import images & icons
 import user_photo from "../../IMG/user_photo.jpg";
 import dropdown_arrow from "../../IMG/dropdown_arrow.svg";
 import close_icon from "../../IMG/close_icon.svg";
@@ -10,37 +10,25 @@ import close_icon from "../../IMG/close_icon.svg";
 const Header = () => {
     return( 
         <header>
-            <nav className={styles.headerNavBar}>
+            <nav className={styles.header_NavBar}>
                 <div>
                     <select name="Organization">
                         <option value="SCHOOL_1">SCHOOL 1</option>
                         <option value="SCHOOL_2">SCHOOL 2</option>
                     </select>
                 </div>
-                <div>
-                    <NavLink to="#">ANALYTICS</NavLink>
-                </div>
-                <div>
-                    <NavLink to="#">GRADEBOOKS</NavLink>
-                </div>
-                <div>
-                    <NavLink to="#">TESTS</NavLink>
-                </div>
-                <div>
-                    <NavLink to="#">STUDENTS</NavLink>
-                </div>
-                <div>
-                    <NavLink to="#">TEACHERS</NavLink>
-                </div>
-                <div>
-                    <NavLink to="#">ARCHIVE</NavLink>
-                </div>
-                <div className={styles.dropdownWrap}>
+                <div><NavLink to="#">ANALYTICS</NavLink></div>
+                <div><NavLink to="#">GRADEBOOKS</NavLink></div>
+                <div><NavLink to="#">TESTS</NavLink></div>
+                <div><NavLink to="#">STUDENTS</NavLink></div>
+                <div><NavLink to="#">TEACHERS</NavLink></div>
+                <div><NavLink to="#">ARCHIVE</NavLink></div>
+                <div className={styles.blockWithIcon}>
                     <div className={styles.loginPhoto}><img src={user_photo} alt="user_photo"/></div>
-                    <div className={styles.dropdown_arrow}><img src={dropdown_arrow} alt="dropdown menu arrow"/></div>
+                    <div className={styles.iconFlexAndSize}><img src={dropdown_arrow} alt="dropdown menu arrow"/></div>
                 </div>
             </nav>
-            <div className={styles.headerSelectBar}>
+            <div className={styles.header_SelectBar}>
                 <select name="SHOW ALL">
                     <option value="SHOW_ALL">SHOW ALL</option>
                 </select>
@@ -59,9 +47,9 @@ const Header = () => {
                 <select name="ALL CLASSES">
                     <option value="ALL_CLASSES">ALL CLASSES</option>
                 </select>
-                <div>
-                    <div><img src={close_icon} alt="close_icon"/></div>
-                    <div><span>CLEAR ALL</span></div>
+                <div className={styles.blockWithIcon}>
+                    <div className={styles.iconFlexAndSize}><img src={close_icon} alt="close_icon"/></div>
+                    <div className={styles.closeButton}><div>CLEAR ALL</div></div>
                 </div>
             </div>
         </header>
